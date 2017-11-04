1d6884f63267e11e53ece6eb7c613a8e7dae28ba
nslam <3150103693@zju.edu.cn>
2017-11-05 00:34:32 +0800

    update

--- interact/admin.py
+++ interact/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from interact.models import *
 
 # Register your models here.
+admin.site.register([MaterialType, Material, Emotion])
--- interact/models.py
+++ interact/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class MaterialType(models.Model):
+    name = models.CharField(max_length=300)
+
+
+class Emotion(models.Model):
+    name = models.CharField(max_length=300)
+
+
+class Material(models.Model):
+    type = models.ForeignKey(MaterialType)
+    url = models.CharField(max_length=300)
+    title = models.CharField(max_length=300)
+    content = models.CharField(max_length=300)
+    picUrl = models.CharField(max_length=300)
+    emotion = models.ManyToManyField(Emotion)
