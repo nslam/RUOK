b73f6430e56db94591f3287df6287aab5977f4a9
awmleer <awmleer@sparker.xyz>
2017-11-05 08:27:26 +0800

    add cors

--- RUOK/settings.py
+++ RUOK/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'interact',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +121,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CORS_ORIGIN_WHITELIST = (
+    'null',
+    'localhost:4200',
+    '127.0.0.1:4200',
+)
+
+CORS_ALLOW_CREDENTIALS = True
+
+
+
+
